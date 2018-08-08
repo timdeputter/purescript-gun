74e41dd572db21b42826d29beb104694f3dda655
Tim de Putter <tim.de.putter83@googlemail.com>
2018-08-08 09:28:11 +0200

    Update Gun.purs

--- src/Gun.purs
+++ src/Gun.purs
@@ -5,7 +5,12 @@ import Effect (Effect)
 
 foreign import data GunDb :: Type
 
+foreign import GunCtx :: Type
+
+
 foreign import syncWithPeer :: String -> Effect GunDb
 
 foreign import syncWithPeers :: Array String -> Effect GunDb
 
+
+foreign import get :: GunDb -> String -> GunCtx
