44b3c0da560cf02aca385b11712cac40c154cdff
Tim de Putter <tim.de.putter83@googlemail.com>
2018-08-08 12:48:10 +0200

    Update Gun.js

--- src/Gun.js
+++ src/Gun.js
@@ -1,3 +1,5 @@
+"use strict";
+
 exports.syncWithPeer = function (url) {
   return function () {
     return Gun(url);
@@ -15,3 +17,13 @@ exports.get = function (gundb, name) {
     return gundb.get(name);
   };
 };
+
+exports.back = = function (gundb, count) {
+  return function () {
+    if( count instanceof exports.NumberOfHops){
+      return gundb.back(count.value0);
+    }
+    return gundb.back(-1);
+  };
+};
+
