263d7a2150ac84f77f5b5ef60b2e9dbfb294d74d
Tim de Putter <tim.de.putter83@googlemail.com>
2018-08-08 20:18:42 +0200

    make it compile

--- src/Gun.js
+++ src/Gun.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// module Gun
+
+var Gun = require('gun/gun');
+
 exports.syncWithPeer = function (url) {
   return function () {
     return Gun(url);
@@ -12,18 +16,21 @@ exports.syncWithPeers = function (urls) {
   };
 };
 
-var get = function (ctx, name) {
+exports.getOnGunDb = function (ctx, name) {
   return function () {
     return ctx.get(name);
   };
 };
 
-exports.getOnGunDb = get;
-exports.getOnChain = get;
-
-exports.back = = function (gundb, count) {
+exports.getOnChain = function (ctx, name) {
   return function () {
-    if( count instanceof exports.NumberOfHops){
+    return ctx.get(name);
+  };
+};
+
+exports.back = function (gundb, count) {
+  return function () {
+    if(count instanceof exports.NumberOfHops){
       return gundb.back(count.value0);
     }
     return gundb.back(-1);
@@ -41,10 +48,11 @@ exports._once = function (ctx) {
     var canceled = false;
     ctx.once(function(data){
       if(!canceled) {
-        if(data === undefined)
-          onSuccess(exports.Nothing.value());
-        else
+        if(data === undefined){
+          onSuccess(exports.Nothing.value0);
+        } else{
           onSuccess(exports.Just.create(data));
+        }
       }
     });
     return function (cancelError, cancelerError, cancelerSuccess) {
--- src/Gun.purs
+++ src/Gun.purs
@@ -1,6 +1,10 @@
 module Gun where
 
 import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Aff.Compat
+import Data.Maybe (Maybe)
+import Control.Semigroupoid ((<<<))
 
 
 data GoBack = Root | NumberOfHops Int
@@ -8,9 +12,9 @@ data GoBack = Root | NumberOfHops Int
 
 foreign import data GunDb :: Type
 
-foreign import GunChainCtx :: Type
+foreign import data GunChainCtx :: Type
 
-foreign import GunRef :: Type -> Type
+foreign import data GunRef :: Type -> Type
 
 
 --Gun(options)
@@ -25,7 +29,7 @@ foreign import syncWithPeers :: Array String -> Effect GunDb
 -- Where to read data from.
 
 class Getable a where
-  get :: a -> String
+  get :: a -> String -> GunChainCtx
 
 instance getGunDb :: Getable GunDb where
   get = getOnGunDb
@@ -45,12 +49,12 @@ foreign import back :: GunChainCtx -> GoBack -> GunChainCtx
 
 -- gun.put(data, callback) 
 -- Save data into gun, syncing it with your connected peers.
-foreign import put :: forall a. GunChainCtx -> a -> Effect GunRef a
+foreign import put :: forall a. GunChainCtx -> a -> Effect (GunRef a)
 
 
 -- gun.once(callback, option)
 -- Get the current data without subscribing to updates. Or undefined if it cannot be found.
-foreign import _once :: forall a. GunChainCtx -> EffectFnAff Maybe a
+foreign import _once :: forall a. GunChainCtx -> EffectFnAff (Maybe a)
 
-once :: forall a. GunChainCtx -> Aff Maybe a
+once :: forall a. GunChainCtx -> Aff (Maybe a)
 once = fromEffectFnAff <<< _once
