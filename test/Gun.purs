6ceea160502638454b5906c75fe94e2b54a0400a
Tim de Putter <tim.de.putter83@googlemail.com>
2018-08-08 20:29:00 +0200

    tests configuriert

--- test/Gun.purs
+++ test/Gun.purs
@@ -1,9 +1,22 @@
-module Test.Gun where
+module Test.Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
+import Test.Spec (pending, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (run)
 
 main :: Effect Unit
-main = do
-  log "You should add some tests."
+main = run [consoleReporter] do
+  describe "purescript-spec" do
+    describe "Attributes" do
+      it "awesome" do
+        let isAwesome = true
+        isAwesome `shouldEqual` true
+      pending "feature complete"
+    describe "Features" do
+      it "runs in NodeJS" $ pure unit
+      it "runs in the browser" $ pure unit
+      it "supports streaming reporters" $ pure unit
+      it "is PureScript 0.10.x compatible" $ pure unit
