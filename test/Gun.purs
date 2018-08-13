6d11b13d726e754886c41c0b7c43bcf21c67e053
Tim de Putter <tim.de.putter83@googlemail.com>
2018-08-13 14:59:36 +0200

    Update Gun.purs

--- test/Gun.purs
+++ test/Gun.purs
@@ -6,17 +6,19 @@ import Test.Spec (pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
+import Gun
 
 main :: Effect Unit
 main = run [consoleReporter] do
-  describe "purescript-spec" do
-    describe "Attributes" do
-      it "awesome" do
-        let isAwesome = true
-        isAwesome `shouldEqual` true
-      pending "feature complete"
-    describe "Features" do
-      it "runs in NodeJS" $ pure unit
-      it "runs in the browser" $ pure unit
-      it "supports streaming reporters" $ pure unit
-      it "is PureScript 0.10.x compatible" $ pure unit
+  describe "purescript-gun" do
+    describe "Gun api" do
+      it "puts data into the gun db" do
+        gundb <- offline
+        let path = gundb # get "users"
+        ctx <-  path # put {name: "John", surname: "Doe"}
+        res <- once 
+        res.name `shouldEqual` "John"        
+      pending "set"
+      pending "map"
+      pending "path"
+
