c6aed30ae34d7eef7fd92dad44d0373ef6bf6875
Tim de Putter <tim.de.putter83@googlemail.com>
2018-08-13 13:21:26 +0200

    Update Sea.purs

--- src/Sea.purs
+++ src/Sea.purs
@@ -3,5 +3,12 @@ module Gun.Sea where
 import Effect (Effect)
 
 
-foreign import data Pair :: Type
+foreign import _create :: GunDb -> String -> String -> EffectFnAff User
 
+foreign import _auth :: GunDb -> String -> String -> EffectFnAff User
+
+auth :: GunDb -> String -> String -> Aff GunChainCtx 
+auth = fromEffectFnAff <<< _auth
+
+create :: GunDb -> String -> String -> Aff GunChainCtx 
+create = fromEffectFnAff <<< _create
