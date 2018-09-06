0809b44c2f672998b0d2a9db857e4bbf60351265
Tim de Putter <tim.de.putter83@googlemail.com>
2018-09-06 11:54:28 +0200

    Update Gun.purs

--- test/Gun.purs
+++ test/Gun.purs
@@ -34,19 +34,15 @@ main = run [consoleReporter] do
         jim <- liftEffect $ gundb # get "jim" # put {name: "jim"}
         _ <- liftEffect $ gundb # get "users" # set john
         _ <- liftEffect $ gundb # get "users" # set jim
-        assertGunResult gundb # get "users" # map # once 
+        assertGunResults (gundb # get "users" # map # once) ["John", "jim"]
 
-      -- !!!!! HIER MUSS WARSCHEINLICH EIN REF (Effect.Ref) BENUTZT WERDEN UM ZU TESTEN !!!!
-      -- it "can subscribe to changes on a chaincontext" do
-      --  gundb <- liftEffect offline
-      --  let p = gundb # get "users"
-      --  liftEffect $ p # on handleUserChanges >>= put {name: "John", surname: "Doe"}
+assertGunResults :: forall a b. Aff (Maybe {data :: {name :: String | a} | b}) -> Array String -> Aff Unit
+assertGunResults aff names = aff >>= \res -> bound res name
+  where
+  bound :: forall c d. Maybe {data :: {name :: String | c} | d} -> String -> Aff Unit
+  bound (Just gunVal) expectedName = gunVal.data.name `shouldEqual` expectedName
+  bound Nothing _ = fail "No result"
 
-      pending "map"
-
--- !!!!! HIER MUSS WARSCHEINLICH EIN REF (Effect.Ref) BENUTZT WERDEN UM ZU TESTEN !!!!
--- handleUserChanges :: forall a. {name :: String | a} -> String -> Effect Unit
--- handleUserChanges {name} expectedName = name `shouldEqual` expectedName 
 
 assertGunResult :: forall a b. Aff (Maybe {data :: {name :: String | a} | b}) -> String -> Aff Unit
 assertGunResult aff name = aff >>= \res -> bound res name
