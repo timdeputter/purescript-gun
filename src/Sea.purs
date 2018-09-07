3d393400a39cb3f815b77ab3bc1139a8c051981e
Tim de Putter <tim.de.putter83@googlemail.com>
2018-09-07 13:24:10 +0200

    GunRoot

--- src/Sea.purs
+++ src/Sea.purs
@@ -5,12 +5,13 @@ import Effect.Aff (Aff)
 import Data.Function.Uncurried (Fn3, runFn3)
 import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 
-foreign import _create :: Fn3 GunDb String String (EffectFnAff GunChainCtx)
 
-foreign import _auth :: Fn3 GunDb String String (EffectFnAff GunChainCtx)
+foreign import _create :: Fn3 GunDb String String (EffectFnAff GunRoot)
 
-auth :: GunDb -> String -> String -> Aff GunChainCtx 
+foreign import _auth :: Fn3 GunDb String String (EffectFnAff GunRoot)
+
+auth :: GunDb -> String -> String -> Aff GunRoot 
 auth gundb usr pwd = fromEffectFnAff (runFn3 _auth gundb usr pwd)
 
-create :: GunDb -> String -> String -> Aff GunChainCtx 
+create :: GunDb -> String -> String -> Aff GunRoot 
 create  gundb usr pwd = fromEffectFnAff (runFn3 _create gundb usr pwd)
