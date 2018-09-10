e96838da57dd85922465d4b9c84b624487e75c48
Tim de Putter <tim.de.putter83@googlemail.com>
2018-09-10 11:15:54 +0200

    Create returns Unit

--- src/Sea.purs
+++ src/Sea.purs
@@ -6,12 +6,12 @@ import Data.Function.Uncurried (Fn3, runFn3)
 import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 
 
-foreign import _create :: Fn3 GunDb String String (EffectFnAff User)
+foreign import _create :: Fn3 GunDb String String (EffectFnAff Unit)
 
 foreign import _auth :: Fn3 GunDb String String (EffectFnAff User)
 
 auth :: GunDb -> String -> String -> Aff User 
 auth gundb usr pwd = fromEffectFnAff (runFn3 _auth gundb usr pwd)
 
-create :: GunDb -> String -> String -> Aff User 
+create :: GunDb -> String -> String -> Aff Unit 
 create  gundb usr pwd = fromEffectFnAff (runFn3 _create gundb usr pwd)
